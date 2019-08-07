3a5b029ca3056de1dbfea0383a30cdbe1bbeebfb
Seth Nadu <seth.nadu@gmail.com>
2019-08-07 16:23:30 -0400

    Styling completed

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -2,32 +2,123 @@ import React, {useState, useEffect} from "react";
 import axios from "axios"
 import {Form, Field, withFormik} from "formik"
 import * as Yup from 'yup';
+import { makeStyles } from '@material-ui/core/styles';
+
+import UserCard from "./user.js";
+
+const useStyles = makeStyles({
+    mainContainer: {
+      margin: "30px"
+    },
+    formContainer: {
+        display: "flex",
+        width: "500px",
+        flexDirection: "column",
+        margin: "auto",
+        alignItems: "center",
+        padding: "20px",
+        backgroundColor: "#fffd9d"
+        },
+    title: {
+      fontSize: "2rem",
+      textDecoration: "underline #00a388",
+      color: "#ff6138"
+    },
+    field: {
+      width: "50%",
+      margin: "10px",
+      height: "20px",
+      borderRadius: "5px",
+      backgroundColor: "#bdeb9f"
+    },
+    checkbox: {
+        display: "flex",
+        flexDirection: "row",
+        
+        margin: "10px 230px 10px 0px",
+    },
+    error: {
+        color: "red",
+        fontSize: ".75rem",
+        marginTop: "0",
+        marginBottom: "10px"
+        
+    },
+    errorCheckbox: {
+        color: "red",
+        fontSize: ".75rem",
+        marginTop: "-23px",
+        marginBottom: "20px",
+        
+    },
+    TOS: {
+        color: "#00a388",
+        fontSize: ".9rem"
+    },
+    button: {
+        width: "80px",
+        padding: "5px 10px",
+        backgroundColor: "#bdeb9f",
+        border: "2px solid #7abd8f",
+        borderRadius: "10px"
+    },
+    userCards: {
+        display: "flex",
+        flexFlow: "row wrap",
+        margin: "10px",
+    }
+
+  });
 
 
-const UserForm = ({ errors, touched, checkbox, handleSubmit, status }) => {
 
 
+const UserForm = ({ errors, touched, checkbox, status }) => {
+    const classes = useStyles();
+
+
+    const [users, setUsers] = useState([{name: "Seth", email:"seth.nadu@gmail.com", createdAt: "placeholder"}]);
+    console.log(users)
+    
+
+    useEffect(() => {
+        if(status) {
+            setUsers([...users, status])
+        }
+    }, [status])
+    console.log(users)
     return (
-        <div>
-          <h1>User Signup</h1>
-          <Form>
-              <Field type = "text" name = "name" placeholder = "Name" />
-              {touched.name && errors.name && <p>{errors.name}</p>}
-              <Field type = "email" name = "email" placeholder = "Email" />
-              {touched.email && errors.email && <p>{errors.email}</p>}
-              <Field type = "password" name = "password" placeholder = "Password" />
-              {touched.password && errors.password && <p>{errors.password}</p>}
-              <Field type="checkbox" name="TOS" checked ={checkbox}/>
-              {touched.TOS && errors.TOS && <p>{errors.TOS}</p>}
-              <button type="submit">Submit</button>
+        <div className={classes.mainContainer}>
+          <div >
+           
+          <Form className={classes.formContainer}>
+           <h2 className={classes.title}>User Signup</h2>      
+              <Field className={classes.field} type = "text" name = "name" placeholder = "Name" />
+              {touched.name && errors.name && <p className={classes.error}>{errors.name}</p>}
+              <Field className={classes.field} type = "email" name = "email" placeholder = "Email" />
+              {touched.email && errors.email && <p className={classes.error}>{errors.email}</p>}
+              <Field className={classes.field} type = "password" name = "password" placeholder = "Password" />
+              {touched.password && errors.password && <p className={classes.error}>{errors.password}</p>}
+              <label className={classes.TOS}>
+              Accept Terms and Conditions
+              <Field className={classes.checkbox} type="checkbox" name="TOS" checked ={checkbox}/>
+              </label>
+              {touched.TOS && errors.TOS && <span className={classes.errorCheckbox}>{errors.TOS}</span>}
+              <button className={classes.button} type="submit">Submit</button>
           </Form>
+          </div>
+           <h2 className={classes.title}>Users</h2> 
+          <div className={classes.userCards}>
+          {users.map(user => {
+             return <UserCard key={user.id} user={user} />
+          })}
+          </div>
         </div>
     )
 }
 
 const FormikUserForm = withFormik({
     mapPropsToValues({name, email, password, TOS}){
-        console.log({name, email, password, TOS})
         return {
             name: name || "",
             email: email || "",
@@ -39,24 +130,26 @@ const FormikUserForm = withFormik({
 
     validationSchema: Yup.object().shape({
         name: Yup.string()
-            .required(),
+            .required("Name is Required"),
         email: Yup.string()
-            .email()
-            .required(),
+            .email("Needs to be an Email")
+            .required("Email is Required"),
         password: Yup.string()
-            .min(8)
-            .required(),
+            .min(8, "Password must be at least 8 characters")
+            .required("Password is Required"),
         TOS: Yup.boolean()
             .oneOf([true], "Must Accept Terms and Conditions")
             .required()
     }),
 
-    handleSubmit(values, {setStatus}) {
+    handleSubmit(values, {resetForm, setStatus}) {
         axios 
             .post('https://reqres.in/api/users/', values)
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
+            .then(res => setStatus(res.data))
+            .catch(err => console.log(err.response))
+        resetForm();
     }
+    
 })(UserForm);
 
 
---
+++ user-onboarding/src/Components/user.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    width: "300px",
+    backgroundColor: "#7abd8f",
+    border: "2px solid #fffd9d"
+  },
+  title: {
+    fontSize: "1.4rem",
+    color: "#ff6138"
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function UserCard({user}) {
+    console.log(user.name)
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className ={classes.title} variant="h5" component="h2">
+          {user.name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {user.email}
+        </Typography>
+        
+      </CardContent>
+      <Typography className={classes.pos} color="textSecondary">
+         <span>Created: </span> {user.createdAt}
+        </Typography>
+    </Card>
+  );
+}
+
+export default UserCard;
